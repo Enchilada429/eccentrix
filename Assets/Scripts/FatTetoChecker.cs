37d1943dea15b4f49bba7b940d9a4b1384c59a7d
Sepehr <seperbozorgy22@gmail.com>
2026-02-20 01:59:59 +0000

    FatTetoChecker will also stop the editor now.

--- Assets/Scripts/FatTetoChecker.cs
+++ Assets/Scripts/FatTetoChecker.cs
@@ -5,12 +5,16 @@ public class FatTetoChecker : MonoBehaviour
 {
 // only check during Editor because it uses the UnityEditor package which will break the build if tried to package while building
 #if UNITY_EDITOR
+
+    private readonly string FAT_TETO_PATH = "Assets/Textures/fat_teto.png";
+
     void Start()
     {
         // check if the fat teto png exists and if not then quit the game (wont work in editor)
-        if (!AssetDatabase.AssetPathExists("Assets/Textures/fat_teto.png"))
+        if (!AssetDatabase.AssetPathExists(FAT_TETO_PATH))
         {
             Debug.Log("<color=red>ERROR: MISSING FAT TETO PNG IN TEXTURES FOLDER</color>");
+            EditorApplication.isPlaying = false;
             Application.Quit();
         }
         else
